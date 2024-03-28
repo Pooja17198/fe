3658a3e30a05c5157ff2980a1d7dc655fdfb98d0
Rakshit Viswanatham <rakshit.viswanatham@oracle.com>
2024-03-28 19:59:14 +0000

    Pull request #2: Remove all java related items to start fresh.
    Merge in NETAUTO/lvv-frontend from rmviswan/remove-all-java-files to master
    
    Squashed commit of the following:
    
    commit e721b8e7524efec1069cf8b6c8f21592eb05d61e
    Author: Rakshit Viswanatham <rakshit.viswanatham@oracle.com>
    Date:   Thu Mar 28 15:57:10 2024 -0400
    
        Update ocibuild.conf
    
    commit a867ff4be95b6500ea213abe0ea4f322dbbc325e
    Author: Rakshit Viswanatham <rakshit.viswanatham@oracle.com>
    Date:   Thu Mar 28 15:55:15 2024 -0400
    
        Update ocibuild.conf
    
    commit 906d3993f4116e9c39c3124c221233d445ea560d
    Author: Rakshit Viswanatham <rakshit.viswanatham@oracle.com>
    Date:   Thu Mar 28 15:53:16 2024 -0400
    
        Update ocibuild.conf
    
    commit 3299b6a0ce7cdd294a29cc65ad78f799ae5e0a7d
    Author: Rakshit Viswanatham <rakshit.viswanatham@oracle.com>
    Date:   Thu Mar 28 15:52:54 2024 -0400
    
        Update ocibuild.conf
    
    commit 76e0a8da7f65114cba4d2ec682fbe6aa2918cafb
    Author: Rakshit Viswanatham <rakshit.viswanatham@oracle.com>
    Date:   Thu Mar 28 15:50:23 2024 -0400
    
        Add package.json
    
    commit cc445d16a4b609c3b521c76ffc73f793c1233a1a
    Author: Rakshit Viswanatham <rakshit.viswanatham@oracle.com>
    Date:   Thu Mar 28 15:43:58 2024 -0400
    
        Update ocibuild.conf
    
    commit 799923d2ba572f54475253809c501ca3fbcfa8a0
    Author: Rakshit Viswanatham <rakshit.viswanatham@oracle.com>
    Date:   Thu Mar 28 15:42:50 2024 -0400
    
        Update readme, ocibuild.conf.
    
    commit f0306e718f2f95971324128fa6e890a5c39bb27c
    Author: Rakshit Viswanatham <rakshit.viswanatham@oracle.com>
    Date:   Thu Mar 28 15:32:42 2024 -0400
    
        Remove all java related items to start fresh.

--- lvv-frontend-api/src/main/java/com/oracle/pic/networking/lvv/frontend/auth/AuthVerbs.java
+++
@@ -1,9 +0,0 @@
-package com.oracle.pic.networking.lvv.frontend.auth;
-
-public class AuthVerbs {
-    public static final String LVV_FRONTEND_INSPECT = "LVV_FRONTEND_INSPECT";
-    public static final String LVV_FRONTEND_READ = "LVV_FRONTEND_READ";
-    public static final String LVV_FRONTEND_CREATE = "LVV_FRONTEND_CREATE";
-    public static final String LVV_FRONTEND_UPDATE = "LVV_FRONTEND_UPDATE";
-    public static final String LVV_FRONTEND_DELETE = "LVV_FRONTEND_DELETE";
-}
