985b5270bf5624618ab40aeb6f9698dcddd96e2e
ansaagra <anshul.v.agrawal@oracle.com>
2026-05-12 07:32:23 -0700

    Revert "Pull request #212: Reduce wait time to 5 mins"
    This reverts commit 82a109468d0b175ecb615fb793021ec2984a7647.

--- src/landing/components/rack/constants.ts
+++ src/landing/components/rack/constants.ts
@@ -6,6 +6,6 @@ export const IDE_API = getIdeApiBase();
 export const VALIDATION_TABLE_ACCESSIBILITY = { rowHeader: "ActionItems" } as const;
 
 export const POLLING = {
-  MAX_ATTEMPTS: 30,
+  MAX_ATTEMPTS: 60,
   INTERVAL_MS: 10000,
 };
