11353766a72ba90e68498c790fb5ac5c14480bf6
Pranamika Hariprasad <pranamika.hariprasad@oracle.com>
2026-05-13 13:42:09 -0400

    Revert "add 128 as page size option"
    This reverts commit a39fde995d6f8e6821c23aff0fd6729efa6af00a.

--- src/landing/components/home/projectDetails.tsx
+++ src/landing/components/home/projectDetails.tsx
@@ -1175,7 +1175,6 @@ const ProjectDetailsContainer = (props: Props) => {
                         <option value="25">25</option>
                         <option value="50">50</option>
                         <option value="100">100</option>
-                        <option value="128">128</option>
                     </select>
                 </label>
             </div>
